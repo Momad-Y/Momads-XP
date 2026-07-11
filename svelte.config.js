cc20601b36ba416475186d84daad6b9626234222
Mohamed Abdelnasser <Mohamed.Y.Abdelnasser@gmail.com>
2026-07-12 00:09:55 +0400

    feat: swap to adapter-netlify and add netlify.toml (Node 22)

--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,10 @@
 import preprocess from "svelte-preprocess";
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      routes: {
-        include: ['/*'],
-        exclude: [
-          '<build>',
-          '/audio/*',
-          '/empty/*',
-          '/favicon.png',
-          '/files/*',
-          '/fonts/*',
-          '/html/*',
-          '/images/*',
-          '/js/*',
-          '/json/*',
-        ]
-      }
-    })
+    adapter: adapter()
   },
   compilerOptions: {
     compatibility: {
